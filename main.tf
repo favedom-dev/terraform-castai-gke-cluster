f5c2ee20970bb2b9229a49f1d6b2295a54325db0
Marcin Kaciuba <marcin.kaciuba@gmail.com>
2022-04-08 10:41:22 +0300

    feat: gke cluster castai module

---
+++ main.tf
@@ -0,0 +1,76 @@
+resource "castai_gke_cluster" "castai_cluster" {
+  project_id                 = var.project_id
+  region                     = var.gke_cluster_region
+  name                       = var.gke_cluster_name
+  ssh_public_key             = var.ssh_public_key
+  delete_nodes_on_disconnect = var.delete_nodes_on_disconnect
+  credentials_json           = var.gke_credentials
+}
+
+resource "helm_release" "castai_agent" {
+  name            = "castai-agent"
+  repository      = "https://castai.github.io/helm-charts"
+  chart           = "castai-agent"
+  namespace       = "castai-agent"
+  create_namespace = true
+  cleanup_on_fail = true
+
+  set {
+    name  = "provider"
+    value = "gke"
+  }
+
+  set {
+    name = "createNamespace"
+    value = "false"
+  }
+
+  dynamic "set" {
+    for_each = var.api_url != "" ? [var.api_url] : []
+    content {
+      name  = "apiURL"
+      value = var.api_url
+    }
+  }
+
+  set_sensitive {
+    name  = "apiKey"
+    value = castai_gke_cluster.castai_cluster.cluster_token
+  }
+}
+
+resource "helm_release" "castai_cluster_controller" {
+  name            = "cluster-controller"
+  repository      = "https://castai.github.io/helm-charts"
+  chart           = "castai-cluster-controller"
+  namespace       = "castai-agent"
+  create_namespace = true
+  cleanup_on_fail = true
+
+  set {
+    name  = "castai.clusterID"
+    value = castai_gke_cluster.castai_cluster.id
+  }
+
+  dynamic "set" {
+    for_each = var.api_url != "" ? [var.api_url] : []
+    content {
+      name  = "castai.apiURL"
+      value = var.api_url
+    }
+  }
+
+  set_sensitive {
+    name  = "castai.apiKey"
+    value = castai_gke_cluster.castai_cluster.cluster_token
+  }
+
+  depends_on = [helm_release.castai_agent]
+}
+
+resource "castai_autoscaler" "castai_autoscaler_policies" {
+  autoscaler_policies_json = var.autoscaler_policies_json
+  cluster_id               = castai_gke_cluster.castai_cluster.id
+
+  depends_on = [helm_release.castai_agent]
+}
---
+++ variables.tf
@@ -0,0 +1,44 @@
+variable "api_url" {
+  type        = string
+  description = "URL of alternative CAST AI API to be used during development or testing"
+  default     = "https://api.cast.ai"
+}
+
+variable "project_id" {
+  type        = string
+  description = "The project id from GCP"
+}
+
+variable "gke_cluster_name" {
+  type        = string
+  description = "Name of the cluster to be connected to CAST AI."
+}
+
+variable "autoscaler_policies_json" {
+  type        = string
+  description = "Optional json object to override CAST AI cluster autoscaler policies"
+  default     = ""
+}
+
+variable "delete_nodes_on_disconnect" {
+  type        = bool
+  description = "Optionally delete Cast AI created nodes when the cluster is destroyed"
+  default     = false
+}
+
+variable "ssh_public_key" {
+  type        = string
+  description = "Optional SSH public key for VM instances. Accepted values are base64 encoded SSH public key or AWS key pair ID"
+  default     = null
+}
+
+variable "gke_cluster_region" {
+  type        = string
+  description = "Region of the cluster to be connected to CAST AI."
+}
+
+variable "gke_credentials" {
+  type = string
+  description = "Optional GCP Service account credentials.json"
+}
+
